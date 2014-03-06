b68028558fc42a557f85f58c6c51e73293362073
Paul Kenny <kenny.paul@gmail.com>
2014-03-06 18:33:47 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Daon Voice Demo
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('daonVoiceAppApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+  });
